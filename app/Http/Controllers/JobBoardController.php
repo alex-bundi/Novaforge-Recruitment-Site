9ddc5ea4edf7a61e48c130846bf9bb122ea8ec3d
Alex Bundi <bundialex45@gmail.com>
2024-01-09 11:09:52 +0300

    New Features

--- app/Http/Controllers/JobBoardController.php
+++ app/Http/Controllers/JobBoardController.php
@@ -13,6 +13,7 @@ class JobBoardController extends Controller
         $job_details = [];
         foreach ($availableJobs as $values){
             $job_details[] = [
+                'job_ID' =>  $values->job_ID,
                 'job_title' => $values->job_title,
                 'job_description' => $values->job_description,
                 'location' => $values->location,
--- resources/js/jobBoard.js
+++ resources/js/jobBoard.js
@@ -1,68 +1,28 @@
 class JobBoard {
-    constructor(){
+    constructor () {
         this.jobList = [];
-        this.url = 'http://127.0.0.1:8000/jobboard/availablejobs';
-
     }
 
-    // Get data from JobBoardController and append to an array
-    getAvailableJobs (){
-        let allJobsHtmlContent = document.getElementById('all__jobs__list');
-        this.name = 'alex';
+    getAvailableJobs () {
+        let requestsCount = 0
+        this.url = 'http://127.0.0.1:8000/jobboard/availablejobs';
         fetch(this.url)
         .then(async (res) => {
-            if(res.ok){
-                let jobsData = await res.json(); // Suspend execution until the returned promise is fulfilled or rejected.
+            if (res.ok) {
+                requestsCount++;
+                console.log(`All requests sent = ${requestsCount}`);
 
-                let message = jobsData.message;
-                let jobDetails = Object.values(message);
+                let jobsData = await res.json();
+                let jobDetails = Object.values(jobsData.message);
+
                 jobDetails.forEach((job) => this.jobList.push(job));
-
-                this.jobList.forEach((job) => {
-                    this.allJobsHtmlContent.innerHTML += `
-                        <div class="flex flex-col mb-2 md:flex-row gap-y-2 md:space-x-4">
-                            <ul>
-                                <li>
-                                    <a href="http://127.0.0.1:8000/jobboard/viewjob">
-                                        <div class="flex p-2 rounded-md border-2 border-slate-200 hover:bg-slate-200 md:basis-2/4">
-                                            
-                                            <div class="flex flex-col">
-                                                <h1 class="pl-3 font-sans text-base font-bold 
-                                                    tracking-wide text-darkBlue md:text-base">
-                                                    ${job.job_title} 
-                                                </h1>
-                                                <div class="flex pl-3 space-x-1">
-                                                    
-                                                    <div class="font-sans text-sm font-semibold 
-                                                        tracking-wide text-black md:text-sm">
-                                                        <h3>Ksh ${job.Salary}</h3>
-                                                    </div>
-                                                    
-                                                    <div class="font-sans text-sm font-semibold 
-                                                        text-black/50 tracking-wide md:text-sm">
-                                                        <P>${job.location} </P>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    `; 
-                });
-   
-                return this.jobList;
-            }else{
-                throw new Error('SQLSTATE[HY000] [2002] No connection could be made because the target machine actively refused it');
+                this.accessJobsData(this.jobList);
             }
+                  
         })
-        .catch((error) => console.log(error))
-                
     }
 
-    // Available Jobs
-    allAvailableJobs () {
+    accessJobsData (data) {
         let submitSearch = false;
         const jobSearchBtn = document.getElementById('search_button');
         
@@ -73,85 +33,172 @@ class JobBoard {
             }
         });
     }
-    
-    // Search for value in records
-    searchForParameter () {
+
+    getSearchParameter () {
         const jobSearchForm = document.getElementById('job_search_form');
+        console.log(jobSearchForm);
+        // jobSearchForm.addEventListener('submit', (event) => {
+        //     event.preventDefault();
+        //     console.log('alex');
+        // })
 
-        jobSearchForm.addEventListener('submit', (event) => {
-            event.preventDefault();
-            let searchInput = document.getElementById('search_jobs').value.trim(); // Clean Input value
+        
+    }
 
-            if (searchInput == null || searchInput.length === 0){
-                this.error('Please fill in the field to search for value...');
-            }else {
-                let value = this.jobList.find(query => query.job_title === searchInput);
 
-                if (typeof value == 'undefined') {
-                    const message = 'Search value not found.';
-                    const notFound = document.getElementById('not_found');
+    // constructor(){
+    //     this.jobList = [];
+    //     this.url = 'http://127.0.0.1:8000/jobboard/availablejobs';
 
-                    if (notFound.classList.contains('invisible')) {
-                        notFound.classList.remove('invisible');
-                      }                    
-                    const paragraph = notFound.querySelector('p'); // Select the <p> element within #not_found
+    // }
+
+    // Get data from JobBoardController and append to an array
+    // getAvailableJobs (){
+    //     let allJobsHtmlContent = document.getElementById('all__jobs__list');
+    //     this.name = 'alex';
+    //     fetch(this.url)
+    //     .then(async (res) => {
+    //         if(res.ok){
+    //             let jobsData = await res.json(); // Suspend execution until the returned promise is fulfilled or rejected.
+
+    //             let message = jobsData.message;
+    //             let jobDetails = Object.values(message);
+    //             jobDetails.forEach((job) => this.jobList.push(job));
+
+    //             this.jobList.forEach((job) => {
+    //                 allJobsHtmlContent.innerHTML += `
+    //                     <div class="flex flex-col mb-2 md:flex-row gap-y-2 md:space-x-4">
+    //                         <ul>
+    //                             <li>
+    //                                 <a href="http://127.0.0.1:8000/jobboard/viewjob">
+    //                                     <div class="flex p-2 rounded-md border-2 border-slate-200 hover:bg-slate-200 md:basis-2/4">
+                                            
+    //                                         <div class="flex flex-col">
+    //                                             <h1 class="pl-3 font-sans text-base font-bold 
+    //                                                 tracking-wide text-darkBlue md:text-base">
+    //                                                 ${job.job_title} 
+    //                                             </h1>
+    //                                             <div class="flex pl-3 space-x-1">
+                                                    
+    //                                                 <div class="font-sans text-sm font-semibold 
+    //                                                     tracking-wide text-black md:text-sm">
+    //                                                     <h3>Ksh ${job.Salary}</h3>
+    //                                                 </div>
+                                                    
+    //                                                 <div class="font-sans text-sm font-semibold 
+    //                                                     text-black/50 tracking-wide md:text-sm">
+    //                                                     <P>${job.location} </P>
+    //                                                 </div>
+    //                                             </div>
+    //                                         </div>
+    //                                     </div>
+    //                                 </a>
+    //                             </li>
+    //                         </ul>
+    //                     </div>
+    //                 `; 
+    //             });
+   
+    //             return this.jobList;
+    //         }else{
+    //             throw new Error('SQLSTATE[HY000] [2002] No connection could be made because the target machine actively refused it');
+    //         }
+    //     })
+    //     .catch((error) => console.log(error))
+                
+    // }
+
+    // // Available Jobs
+    // allAvailableJobs () {
+    //     let submitSearch = false;
+    //     const jobSearchBtn = document.getElementById('search_button');
+        
+    //     jobSearchBtn.addEventListener('click', () => {
+    //         submitSearch = true;
+    //         if (submitSearch == true) {
+    //             this.searchForParameter();
+    //         }
+    //     });
+    // }
+    
+    // // Search for value in records
+    // searchForParameter () {
+    //     const jobSearchForm = document.getElementById('job_search_form');
+
+    //     jobSearchForm.addEventListener('submit', (event) => {
+    //         event.preventDefault();
+    //         let searchInput = document.getElementById('search_jobs').value.trim(); // Clean Input value
+
+    //         if (searchInput == null || searchInput.length === 0){
+    //             this.error('Please fill in the field to search for value...');
+    //         }else {
+    //             let value = this.jobList.find(query => query.job_title === searchInput);
+
+    //             if (typeof value == 'undefined') {
+    //                 const message = 'Search value not found.';
+    //                 const notFound = document.getElementById('not_found');
+
+    //                 if (notFound.classList.contains('invisible')) {
+    //                     notFound.classList.remove('invisible');
+    //                   }                    
+    //                 const paragraph = notFound.querySelector('p'); // Select the <p> element within #not_found
                     
-                    paragraph.textContent = message; 
-                    setTimeout(() => notFound.remove(), 7000); // Remove warning after 3 secs
+    //                 paragraph.textContent = message; 
+    //                 setTimeout(() => notFound.remove(), 7000); // Remove warning after 3 secs
 
-                }else {
-                    let foundSearchValue = document.getElementById('found_search_value');
+    //             }else {
+    //                 let foundSearchValue = document.getElementById('found_search_value');
 
-                    let searchHtmlContent = `
-                        <div class="flex flex-col md:flex-row gap-y-2 md:space-x-4">
-                            <ul>
-                                <li>
-                                    <div class="flex p-2 rounded-md border-2 border-slate-200 md:basis-2/4">
+    //                 let searchHtmlContent = `
+    //                     <div class="flex flex-col md:flex-row gap-y-2 md:space-x-4">
+    //                         <ul>
+    //                             <li>
+    //                                 <div class="flex p-2 rounded-md border-2 border-slate-200 md:basis-2/4">
                                         
-                                        <div class="flex flex-col">
-                                            <h1 class="pl-3 font-sans text-base font-bold 
-                                                tracking-wide text-darkBlue md:text-base">
-                                                ${value.job_title} 
-                                            </h1>
-                                            <div class="flex pl-3 space-x-1">
+    //                                     <div class="flex flex-col">
+    //                                         <h1 class="pl-3 font-sans text-base font-bold 
+    //                                             tracking-wide text-darkBlue md:text-base">
+    //                                             ${value.job_title} 
+    //                                         </h1>
+    //                                         <div class="flex pl-3 space-x-1">
                                                 
-                                                <div class="font-sans text-sm font-semibold 
-                                                    tracking-wide text-black md:text-sm">
-                                                    <h3>Ksh ${value.Salary}</h3>
-                                                </div>
+    //                                             <div class="font-sans text-sm font-semibold 
+    //                                                 tracking-wide text-black md:text-sm">
+    //                                                 <h3>Ksh ${value.Salary}</h3>
+    //                                             </div>
                                                 
-                                                <div class="font-sans text-sm font-semibold 
-                                                    text-black/50 tracking-wide md:text-sm">
-                                                    <P>${value.location} </P>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    `;
-                    foundSearchValue.innerHTML = searchHtmlContent;
-                }
-            }
-        });
-    }
+    //                                             <div class="font-sans text-sm font-semibold 
+    //                                                 text-black/50 tracking-wide md:text-sm">
+    //                                                 <P>${value.location} </P>
+    //                                             </div>
+    //                                         </div>
+    //                                     </div>
+    //                                 </div>
+    //                             </li>
+    //                         </ul>
+    //                     </div>
+    //                 `;
+    //                 foundSearchValue.innerHTML = searchHtmlContent;
+    //             }
+    //         }
+    //     });
+    // }
 
-    // Display error message
-    error (message){
-        this.message = message;
-        const errorMessageElement = document.getElementById('error_message');
+    // // Display error message
+    // error (message){
+    //     this.message = message;
+    //     const errorMessageElement = document.getElementById('error_message');
 
-        errorMessageElement.textContent = this.message;
-        errorMessageElement.style.visibility = "visible";
-        errorMessageElement.style.color = "red";
+    //     errorMessageElement.textContent = this.message;
+    //     errorMessageElement.style.visibility = "visible";
+    //     errorMessageElement.style.color = "red";
 
-        setTimeout(() => errorMessageElement.remove(), 3000); // Remove warning after 3 secs
-    }
+    //     setTimeout(() => errorMessageElement.remove(), 3000); // Remove warning after 3 secs
+    // }
 
-    selectedJob () {
-        console.log(this.allJobsHtmlContent);
-    }
+    // selectedJob () {
+    //     console.log(this.name)
+    // }
 
 
 
@@ -161,5 +208,4 @@ class JobBoard {
 
 let careersList = new JobBoard();
 careersList.getAvailableJobs();
-careersList.allAvailableJobs();
-careersList.selectedJob();
+// careersList.allAvailableJobs();
