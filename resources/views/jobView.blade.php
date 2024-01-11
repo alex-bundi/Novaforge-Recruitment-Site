db2a36c6029eaa5f7c443551a2cdc53945ae19bd
Alex Bundi <bundialex45@gmail.com>
2024-01-11 08:38:21 +0300

    View Customization

--- resources/views/jobView.blade.php
+++ resources/views/jobView.blade.php
@@ -40,9 +40,31 @@
                 text-black/75 tracking-wide md:text-sm">
                 <P> {{ $selectedCareersDetails['location']  }}</P>
             </div>
-        
         </div>
     </div>
+</section>
+
+<!-- Job Details -->
+<section>
+    <div class="flex  mt-3">
+        <h1
+            class="m-0 font-sans text-xl font-bold tracking-widest md:text-2xl">
+            Description
+        </h1>
+    </div>
+    <div
+        class="p-2 font-sans text-sm tracking-widest md:text-base">
+        <p>{{ $selectedCareersDetails['job_description']  }}</p>
+    </div>
+     <!-- Application Button -->
+    <div class="flex mt-8">
+        <a href=""
+            class="flex justify-center basis-full rounded-lg p-2 bg-darkBlue text-white font-mono font-bold
+                tracking-wide hover:bg-lightBlue md:basis-1/4">
+            Apply Now
+        </a>
+    </div>
+
 </section>
 
 @stop
